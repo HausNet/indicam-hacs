e2d16e4779405078ed5ae01851069efc77feddc8
Louis <louis@hausnet.io>
2024-10-16 16:40:44 -0400

    Evolved tracker script; Added requirements file for testing use

--- bin/track-hass.sh
+++ bin/track-hass.sh
@@ -6,11 +6,30 @@
 # TODO: Figure out how to check out the current lead Beta
 #
 
-# Fresh clone
-git clone git@gitlab.com:hausnet/hass.git ~/tmp/hass
-git checkout master
-# Get latest changes
+# Empty out workspace
+cd ~/workspace/hausnet
+rm -rf ./indicam-track-hass
+mkdir ./indicam-track-hass
+cd ./indicam-track-hass
+
+# Get indicam HACS source
+git clone git@github.com:HausNet/indicam-hacs.git
+
+# Fresh clone of HASS
+git clone git@gitlab.com:hausnet/hass.git 
+cd hass
+git checkout dev
+
+# Get latest upstream & check in
+git remote add upstream https://github.com/home-assistant/home-assistant.git
 git fetch upstream
-git merge upstream/master
-git checkout indicam
+git merge upstream/dev
+git push
+
+# Set up, then run tests
+./script/setup
+. ./venv/bin/activate
+mkdir ./config/custom_components
+ln -s ../../../indicam-hacs/custom_components/indicam ./config/custom_components/indicam
+pip3 install ../indicam-hacs/requirements.txt
 
